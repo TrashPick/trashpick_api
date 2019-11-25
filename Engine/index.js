9c4a09ff96e2d21d0159c2a04c7a303ba2ce3529
Mushud Kofi Abu <mushudkofiabuu@gmail.com>
2019-11-25 22:57:13 +0000

    game Engine

---
+++ Engine/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+	generatedNumber: function() {
+		return Math.floor(Math.random() * 10);
+	},
+
+	newGame: function() {
+		const time = new Date();
+		//	console.log(time);
+	}
+};
