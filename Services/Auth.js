4a8f6d6858fffbd168cf990566ccfd6568ed172c
Mushud Kofi Abu <mushudkofiabuu@gmail.com>
2019-11-25 22:58:12 +0000

    Auth functions of signin and signup

---
+++ Services/Auth.js
@@ -0,0 +1,52 @@
+const User = require('../Models/User');
+
+module.exports = {
+	signup: async ({ mobileNumber, password, country, mobileNetwork }) => {
+		let user = new User({
+			mobileNumber,
+			mobileNetwork,
+			country
+		});
+
+		user.generateID(mobileNumber);
+		user.hashPassword(password);
+
+		if (country === undefined) {
+			return { error: { type: 'client', message: 'Please provide Country' } };
+		} else if (mobileNetwork === undefined) {
+			return {
+				error: { type: 'client', message: 'Please provide Mobile Network' }
+			};
+		} else {
+			await user.save();
+			return {
+				success: {
+					type: 'authsuccess',
+					message: 'User successfully registered',
+					token: user.createWebToken()
+				}
+			};
+		}
+	},
+
+	signin: async (phone, password) => {
+		// Check db for phone number;
+		let user = await User.findOne({ mobileNumber: phone });
+		if (user !== null) {
+			//User is in Db
+			if (user.checkPassword(user.password)) {
+				return {
+					success: {
+						type: 'authsuccess',
+						message: 'User successfully registered',
+						token: user.createWebToken()
+					}
+				};
+			} else {
+				return { error: { type: 'client', message: 'Invalid password' } };
+			}
+		} else {
+			return { error: { type: 'client', message: 'Invalid Phone Number' } };
+		}
+	}
+};
