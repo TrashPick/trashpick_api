f8246942722ef1f70970d63cb067fb9a9c6687b3
Mushud <mushudkofiabuu@gmail.com>
2020-04-16 12:44:25 -0700

    Added route to get donations

--- Routes/UserRoute.js
+++ Routes/UserRoute.js
@@ -1,7 +1,12 @@
 const { Router } = require("express");
 const Auth = require("../Services/Auth");
 const { getUserDataToken } = require("../Services");
-const { rechargeCredits, getUser, donate } = require("../Services/User");
+const {
+  rechargeCredits,
+  getUser,
+  donate,
+  getDonations,
+} = require("../Services/User");
 //const { acknowledgePayment } = require("../Services/Pledge");
 
 const userRoute = Router();
@@ -34,9 +39,18 @@ userRoute.post("/auth/signin", async (req, res) => {
   }
 });
 
-userRoute.get("/auth/me/userID", async (req, res) => {
+userRoute.get("/auth/me/:userID", async (req, res) => {
   const userData = await Auth.getUserData(req.params.userID);
+
   res.status(userData.status).send(userData.data);
+});
+
+userRoute.get("/me/donations/:userID", async (req, res) => {
+  const requestsAndDonations = await getDonations({
+    userID: req.params.userID,
+  });
+
+  res.send(requestsAndDonations);
 });
 
 userRoute.post("/me/recharge", async (req, res) => {
