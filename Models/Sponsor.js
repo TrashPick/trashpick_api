aaa84dc1cb5ec22a5a0d8e0ca78319489f4d3170
Mushud <mushudkofiabuu@gmail.com>
2020-05-22 14:21:12 +0000

    Added Major field to Sponsor

--- Models/Sponsor.js
+++ Models/Sponsor.js
@@ -4,6 +4,7 @@ const Sponsor = new Schema({
   imgUrl: { type: String },
   url: { type: String },
   name: { type: String },
+  major: { type: Boolean, default: false },
 });
 
 module.exports = model("Sponsors", Sponsor);
